4dc2a16b8e699d43d902c5c55540a73faaa02888
Jan Vlnas <git.noreply@jan.vlnas.cz>
2021-09-23 13:12:33 +0200

    Use tha bash

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
-set -o errexit
-set -o nounset
+#!/bin/bash
+set -euo pipefail
+shopt -s globstar
 
+rm -rf public/
 npm run build
 # reveal-md generates slides.md by default
-find _static/ -iname "slides.html" -exec rename slides index '{}' \;
+for f in _static/**/slides.html; do
+  mv $f "${f/slides/index}"
+done
 mv _static public
 rm public/index.html
 cp README.md public/index.md
