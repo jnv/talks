3ff7eef0e64509199c5da22b84751f2a7b9a0805
Jan Vlnas <git.noreply@jan.vlnas.cz>
2021-09-23 12:51:18 +0200

    Rename generates reveal slides

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ set -o errexit
 set -o nounset
 
 npm run build
+# reveal-md generates slides.md by default
+find _static/ -iname "slides.html" -exec rename slides index '{}' \;
 mv _static public
 rm public/index.html
 cp README.md public/index.md
